a3ea9d1c7668397b700b5562433523dac4884021
Aakash Gandhi <aakash@cloudwiry.com>
2023-02-20 22:49:23 +0530

    Fix UI Issues

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { NavLink } from 'react-router-dom'
 import PictureGrid from '../components/PictureGrid'
 import { useAppSelector } from '../redux/hooks'
